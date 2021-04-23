c5044512e2819992cf9ccea1ace0e9835138b695
PetrovaNina <petrova1nina@gmail.com>
2021-04-23 17:17:05 +0300

    Hide navbar after click a link

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-function openNav() {
-    let burger = document.querySelector(".header-burger");
-    let nav = document.getElementById("nav-bar");
-    burger.onclick = function() {
-        addActive(burger, nav)
+let burger = document.querySelector(".header-burger");
+let nav = document.getElementById("nav-bar");
+let navlinks = document.querySelectorAll(".nav-link");
+
+// Show navbar after click the burger
+burger.onclick = function() {
+    burger.classList.toggle("active");
+    nav.classList.toggle("active");
+}
+
+// Hide navbar after click a link
+for (let link of navlinks) {
+    link.onclick = () => {
+        burger.classList.remove("active");
+        nav.classList.remove("active");
     }
 }
-function addActive(first, second) {
-    first.classList.toggle("active");
-    second.classList.toggle("active");
-}
-openNav();
